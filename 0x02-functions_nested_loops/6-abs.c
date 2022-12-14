17e9b4f7325a95d5529bbb6aed092d4338e8d690
omarelshaer2 <omarelshaer2@gmail.com>
2022-12-13 17:48:58 -0800

    gpqq

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * _abs - returns absolute value of an int
- * @n: integer from which to get an absolute value
- * Return: Void.
- */
+* _abs - returns absolute value of an int
+* @n: integer from which to get an absolute value
+* Return: Void.
+*/
 
 int _abs(int n)
 {
