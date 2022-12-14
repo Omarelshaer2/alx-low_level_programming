42e64f922d9b53562974493c67b273d173fdb8e1
omarelshaer2 <omarelshaer2@gmail.com>
2022-12-13 17:23:50 -0800

    llao

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,23 @@
-#include "main.h"                                                                                               /**
- * _isalpha - check the code for ALX School students.
+#include "main.h"
+
+/**
+ * _isalpha - Return 1 if c is a letter. lowercase or uppercase
  *
- * @c: is c an integer argument
- *                                                       * Return: Always 0.                                     */                                                                                                             int _isalpha(int c)
-{                                                       
-        if (c >= 97 && c <= 122)                                {                                                               return (1);                                     }
-        else if (c >= 65 && c <= 90)
-        {
-                return (1);
-        }
-        return (0);
+ *@c: The int to print
+ * Return: Always 0.
+ */
+
+int _isalpha(int c)
+
+{
+
+if ((c > 'a' && c < 'z') || (c > 'A' && c < 'Z'))
+{
+return (1);
+}
+
+else
+{
+return (0);                                             }
+
 }
