45222b99601802300e41eda2691937d03f831742
Omar Elshaer <78622723+Omarelshaer2@users.noreply.github.com>
2023-07-16 02:21:47 +0300

    Update 1-binary.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,15 +1,9 @@
 #include "search_algos.h"
 
 /**
- * binary_search - function that searches for array value in array sorted array
- * of integers using Binary search algorithm.
  * @array: pointer to the first element of the array to search in.
  * @size: number of elements in array.
- * @value: value to search for.
- *
- * You can assume that value won’t appear more than once in array.
- * If value is not present in array or if array is NULL return -1.
- *
+ * @value: value to search for
  * Return: the index where value is located.
  */
 int binary_search(int *array, size_t size, int value)
@@ -18,15 +12,10 @@ int binary_search(int *array, size_t size, int value)
 }
 
 /**
- * binary_search_index - recursive function for helping binary_search.
  * @array: pointer to the first element of the array to search in.
  * @l: left index of array.
  * @r: right index of array.
  * @value: value to look for.
- *
- * You can assume that value won’t appear more than once in array.
- * If value is not present in array or if array is NULL return -1.
- *
  * Return: the index where value is located.
  */
 int binary_search_index(int *array, size_t l, size_t r, int value)
@@ -44,25 +33,15 @@ int binary_search_index(int *array, size_t l, size_t r, int value)
 	if (l == r)
 		return (*(array + mid) == value ? (int)mid : -1);
 
-	/* if we've found the element at the mid-way index, return the index */
 	if (array[mid] == value)
 		return (mid);
-	/**
-	 * else if the element MUST be in the left-portion of the portion of the
-	 * array we are currently looking at, search for it in this portion
-	 */
 	else if (array[mid] > value)
 		return (binary_search_index(array, l, mid - 1, value));
-	/**
-	 * else if the element MUST be in the right-portion of the portion of the
-	 * array we are currently looking at, search for it in this portion
-	 */
 	else
 		return (binary_search_index(array, mid + 1, r, value));
 }
 
 /**
- * print_array - Prints an array of integers.
  * @array: The array to be printed.
  * @l: The left index of the array.
  * @r: The right index of the array.
